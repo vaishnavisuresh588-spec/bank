3d3ce705b359f00442630af50f2bc4926376ab99
vaishnavisuresh588 <vaishnavisuresh588@gami.com>
2025-10-04 11:18:33 +0530

    save

---
+++ bank.py
@@ -0,0 +1,4 @@
+initial_balance = float(input("Enter initial balance: "))
+deposit = float(input("Enter amount to deposit: "))
+updated_balance = initial_balance + deposit
+print("Updated balance after deposit is:", updated_balance)
