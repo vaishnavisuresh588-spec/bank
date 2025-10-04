627ba413419a7166deff5807b4d17719b6bc7d22
vaishnavisuresh588 <vaishnavisuresh588@gami.com>
2025-10-04 11:22:17 +0530

    hello

--- bank.py
+++ bank.py
@@ -1,4 +1,17 @@
 initial_balance = float(input("Enter initial balance: "))
-deposit = float(input("Enter amount to deposit: "))
-updated_balance = initial_balance + deposit
-print("Updated balance after deposit is:", updated_balance)
+transaction_type = input("Enter transaction type (deposit/withdraw): ").lower()
+amount = float(input("Enter amount: "))
+
+if transaction_type == "deposit":
+    updated_balance = initial_balance + amount
+elif transaction_type == "withdraw":
+    if amount > initial_balance:
+        print("Insufficient balance!")
+        updated_balance = initial_balance
+    else:
+        updated_balance = initial_balance - amount
+else:
+    print("Invalid transaction type!")
+    updated_balance = initial_balance
+
+print("Updated balance is:", updated_balance)
